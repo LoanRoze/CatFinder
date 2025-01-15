33d90a19ca9e9f53e15288e73367e2b1a25cac4b
Muffin <loan.roze@efrei.net>
2025-01-15 16:41:17 +0100

    Option de création de compte, redirection auto

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php 
 
 const DB_CONFIG = [
-    'host' => '5.tcp.eu.ngrok.io',
-    'port' => 12426,
+    'host' => '6.tcp.eu.ngrok.io',
+    'port' => 12428,
     'database' => 'catfinder',
     'username' => 'user_php',
     'password' => 'admin',
