22785238376e0933e1e89a29142578e483b880e0
RapidoLaFleche <junioryovo2002@gmail.com>
2025-01-17 11:44:49 +0100

    fix header manquand sur page postAnnoncement

--- views/layout.php
+++ views/layout.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,15 +13,16 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
     <?php foreach ($styles as $style) { ?>
-        <link rel="stylesheet" href="style/<?=$style?>">
+    <link rel="stylesheet" href="style/<?=$style?>">
     <?php } ?>
 
     <script src="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.js" defer></script>
-    
+
     <?php foreach ($scripts as $script) { ?>
-        <script src="script/<?=$script?>" defer></script>
+    <script src="script/<?=$script?>" defer></script>
     <?php } ?>
 </head>
+
 <body>
     <?php require('views/partials/_header.php') ?>
     <main>
@@ -29,4 +31,5 @@
     <?php require('views/partials/_footer.html') ?>
     <?php require('views/partials/_successalert.html') ?>
 </body>
+
 </html>
